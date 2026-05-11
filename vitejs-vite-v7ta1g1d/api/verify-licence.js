bf46140ae4900b2d88b2c8d9be552429c3b1f4e1
veggiepatchideas <veggiepatchideas@gmail.com>
2026-05-11 14:01:40 +0100

    Update verify-licence.js

--- vitejs-vite-v7ta1g1d/api/verify-licence.js
+++ vitejs-vite-v7ta1g1d/api/verify-licence.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: new URLSearchParams({
-        access_token: "R8tILozaagu1-xC2y5LgeRPtU7wGUKYkquQcNhQcyHk",
+        access_token: "cKPz6npiugX2gBau-Gg6RJ-B-rnALCF2fFvz51ejUvE",
         product_id: "iqvXTpQi-68wMeLvpNBlvg==",
         license_key: license_key.trim(),
         increment_uses_count: "false"
